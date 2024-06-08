964cef69b1151d25056c409355e5fd9a72ac6c50
CecedeKP <bamamounicolas@gmail.com>
2024-06-08 21:59:53 +0800

    Change the hostname

--- include/Global_Variables.h
+++ include/Global_Variables.h
@@ -5,10 +5,14 @@
 #ifndef __Global_Variables_H__
 #define __Global_Variables_H__
 #include <Arduino.h>
-const char *hostname = "myEsp32";
+/* const char *hostname = "myEsp32";
 const char* ssid = "xiangyu801";
-const char* password = "xiangyu801";
+const char* password = "xiangyu801"; */
 
+
+const char* hostname = "Nicolas";
+const char* ssid = "FLYRouter-e920";
+const char* password = "b33cd675"; 
 
 #endif
 
